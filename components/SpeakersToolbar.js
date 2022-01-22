b06918d7cfab5e5e565433c637fedfab27727d9e
SuphawitCE <b4nknaja@hotmail.com>
2022-01-22 19:29:01 +0700

    feat(toolbar): Add local state in Toolbar component

--- components/SpeakersToolbar.js
+++ components/SpeakersToolbar.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SpeakersToolbar = () => {
+  const [local, setLocal] = useState({
+    showSessions: true,
+    theme: "light",
+  });
+
+  const { showSessions, theme } = local;
+
+  const handleSessionChange = (e) => {
+    setLocal((prevState) => ({
+      ...prevState,
+      showSessions: e.target.checked,
+    }));
+  };
+
+  const handleThemeChange = (e) => {
+    setLocal((prevState) => ({
+      ...prevState,
+      theme: e.target.value,
+    }));
+  };
+
   return (
     <section className="toolbar dark-theme-header">
       <div className="container">
@@ -9,14 +30,22 @@ const SpeakersToolbar = () => {
             <li className="d-flex flex-column flex-md-row">
               <b>Show Sessions&nbsp;&nbsp;</b>
               <label className="fav">
-                <input type="checkbox" checked={true} />
+                <input
+                  type="checkbox"
+                  checked={showSessions}
+                  onChange={handleSessionChange}
+                />
                 <span className="switch"></span>
               </label>
             </li>
             <li className="d-flex flex-column flex-md-row ml-sm-5 ml-0">
               <strong>Theme</strong>
               <label className="dropdown">
-                <select className="form-control theme" value="light">
+                <select
+                  className="form-control theme"
+                  value={theme}
+                  onChange={handleThemeChange}
+                >
                   <option value="light">Light</option>
                   <option value="dark">Dark</option>
                 </select>
