0d8d179be534d4333fc28d071e16f891c6a6d285
SuphawitCE <b4nknaja@hotmail.com>
2022-01-29 14:56:33 +0700

    fix(Speakers): Remove passing 'theme', 'setParentState' as a props on a Speakers component

--- components/Speakers.js
+++ components/Speakers.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import SpeakersList from "./SpeakersList";
 import SpeakersToolbar from "./SpeakersToolbar";
+import { ThemeContext } from "./App";
 
-const Speakers = ({ theme, showSessions, setParentState }) => {
+const Speakers = (/*{ theme, showSessions, setParentState } */) => {
+  const { showSessions } = useContext(ThemeContext);
   return (
     <>
       <SpeakersToolbar
-        theme={theme}
-        setParentState={setParentState}
-        showSessions={showSessions}
+      // theme={theme}
+      // setParentState={setParentState}
+      // showSessions={showSessions}
       />
       <SpeakersList showSessions={showSessions} />
     </>
