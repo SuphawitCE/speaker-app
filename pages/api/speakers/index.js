f5afa32e841316d269f2af966c95b4f3aca7e963
SuphawitCE <b4nknaja@hotmail.com>
2022-02-06 16:58:42 +0700

    feat(api-route): Implement API route from Next.js & REST GET api routes

---
+++ pages/api/speakers/index.js
@@ -0,0 +1,30 @@
+// import { data } from "../../../SpeakerData";
+import path from "path";
+import fs from "fs";
+// import { stringify } from "querystring";
+
+const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
+const delay = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+
+export default async function handler(req, res) {
+  // res.status(200).send(JSON.stringify(data, null, 2));
+
+  const jsonFile = path.resolve("./", "db.json");
+  try {
+    const readFileData = await readFile(jsonFile);
+    await delay(1000);
+    const speakers = JSON.parse(readFileData).speakers;
+    if (speakers) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(speakers, null, 2));
+      console.log("GET /api/speakers status: 200");
+    }
+  } catch (e) {
+    console.log("/api/speakers error: ", e);
+    res.status(404).send("File not found on server");
+  }
+}
